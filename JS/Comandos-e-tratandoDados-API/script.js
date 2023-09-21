ee4d3288e1d053b1bb1cff8cc02aadebbdbe80cf
nadiawiny <nadiawiny@hotmail.com>
2023-09-21 11:16:17 -0300

    C-API

---
+++ JS/Comandos-e-tratandoDados-API/script.js
@@ -0,0 +1,12 @@
+console.log('Mandando oi pro amigo!')
+
+function mandandoMensagem(){
+    console.log('tudo bem?')
+    console.log('Vou te mandar uma solicitação!')
+    console.log('solicitação recebida!')
+
+}
+
+mandandoMensagem()
+
+console.log('tchau, tchau!')
